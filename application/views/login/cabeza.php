600eb4a2498dcad3986ff178998bba721f850a3e
SAUL NELSON CONDORI <sentoki5555@gmail.com>
2021-10-25 15:48:11 -0400

    mejorando interfas listas

--- application/views/login/cabeza.php
+++ application/views/login/cabeza.php
@@ -5,6 +5,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
